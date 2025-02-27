e246be01cdc1bf88e1931584f6c676b5eee84d83
Excalibur <104138693+DSR3164@users.noreply.github.com>
2025-02-28 02:17:16 +0700

    Added Monokai Theme

--- app/src/main/java/com/example/commapps/ui/theme/Color.kt
+++ app/src/main/java/com/example/commapps/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.example.commapps.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Основные цвета темы Monokai
+val MonokaiBackground = Color(0xFF272822)
+val MonokaiForeground = Color(0xFFF8F8F2)
+val MonokaiBlue = Color(0xFF66D9EF)      
+val MonokaiGreen = Color(0xFFA6E22E)     
+val MonokaiRed = Color(0xFFF92672)       
+val MonokaiOrange = Color(0xFFFD971F)    
+val MonokaiPurple = Color(0xFFAE81FF)    
